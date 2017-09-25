cb4602600f04c7ecd00776c4137d5ba77df00bb9
Nikolay Pavlov <Nikolay@Niki-Laptop.Kodar.local>
2017-09-25 17:33:48 +0300

    First commit

---
+++ Lecture 1/ABranchPrediction/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Diagnostics;
+
+class Program
+{
+    static int dataSize = 10_000_000;
+    static int[] data;
+
+    static void GenerateData()
+    {
+        data = new int[dataSize];
+        Random rand = new Random();
+        for (int i = 0; i < dataSize; i++)
+        {
+            data[i] = rand.Next(10);
+        }
+    }
+
+    static int CountLess(int boundary)
+    {
+        int result = 0;
+        for (int i = 0; i < dataSize; i++)
+        {
+            if (data[i] < boundary) result++;
+            #region Alternative
+            //result = result - ((data[i] - boundary) >> 31);
+            #endregion
+        }
+        return result;
+    }
+
+    static void Main(string[] args)
+    {
+        GenerateData();
+        Stopwatch watch = new Stopwatch();
+        for (int i = 0; i < 11; i++)
+        {
+            watch.Restart();
+            var ops = CountLess(i);
+            watch.Stop();
+            Console.WriteLine($"{i,2}. Count: {ops,8}, Time: {watch.ElapsedTicks,7}");
+        }
+        Console.WriteLine("Ready");
+        Console.ReadLine();
+    }
+}
